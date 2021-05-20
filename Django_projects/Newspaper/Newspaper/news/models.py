822c02e73b4a7233ae973f76e76b4ad9f44046f3
RightSteered <hamsters.corp@yandex.ru>
2021-05-20 23:26:14 +0300

    Rating update does not work :(

---
+++ Django_projects/Newspaper/Newspaper/news/models.py
@@ -0,0 +1,72 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.db.models import Sum
+
+
+class Author(models.Model):
+    user_name = models.OneToOneField(User, on_delete=models.CASCADE)
+    user_rating = models.SmallIntegerField(default=0)
+
+    def update_rating(self):
+        postRat = self.user_name.post_set.aggregate(postRating=Sum('rating'))
+        pRat = 0
+        pRat += postRat.get('postRating')
+
+        commRat = self.user_name.comment_set.aggregate(commRating=Sum('commentRating'))
+        cRat = 0
+        cRat += commRat.get('commRating')
+
+        self.ratingAuthor = pRat * 3 + cRat
+        self.save()
+
+
+class Category(models.Model):
+    cat_id = models.CharField(max_length=64, unique=True)
+
+
+class Post(models.Model):
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    NEWS = 'NW'
+    ARTICLE = 'ART'
+    CATEGORY_CHOICES = [
+        (NEWS, 'Новость'),
+        (ARTICLE, 'Статья')
+    ]
+    cat_type = models.CharField(max_length=3, choices=CATEGORY_CHOICES, default=ARTICLE)
+    created = models.DateTimeField(auto_now_add=True)
+    postCategory = models.ManyToManyField(Category, through='PostCategory')
+    title = models.CharField(max_length=128)
+    text = models.TextField()
+    rating = models.SmallIntegerField(default=0)
+
+    def like(self):
+        self.rating += 1
+        self.save()
+
+    def dislike(self):
+        self.rating -= 1
+        self.save()
+
+    def preview(self):
+        return self.text[0:123] + "..."
+
+
+class Comment(models.Model):
+    commentPost = models.ForeignKey(Post, on_delete=models.CASCADE)
+    commentAuthor = models.ForeignKey(User, on_delete=models.CASCADE)
+    commentText = models.TextField()
+    commentDate = models.DateTimeField(auto_now_add=True)
+    commentRating = models.SmallIntegerField(default=0)
+
+    def like(self):
+        self.commentRating += 1
+        self.save()
+
+    def dislike(self):
+        self.commentRating -= 1
+        self.save()
+
+
+class PostCategory(models.Model):
+    postThrough = models.ForeignKey(Post, on_delete=models.CASCADE)
+    catThrough = models.ForeignKey(Category, on_delete=models.CASCADE)
